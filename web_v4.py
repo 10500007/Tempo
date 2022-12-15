6910563a9676b00c225f5ae34318da7807ee49e6
10500007 <felipe_augusto.bn@hotmail.com>
2022-12-15 19:10:15 -0300

    la vai

---
+++ web_v4.py
@@ -0,0 +1,27 @@
+import requests
+import pprint
+import urllib.parse
+import urllib3.connection
+import urls_localidades
+
+url_fixo = 'https://www.netclima.com.br/php/hi/netclima_recuperahida.php?parametros={'
+url_parametros = urls_localidades.url_tocantins_2
+url_entidade='}&entidade=EDP_T'
+
+encodedStr = url_parametros
+#conv = urllib.parse.unquote(encodedStr)
+#converter formato para link final da url
+url_convert = urllib.parse.quote(encodedStr).replace('%3A',':').replace('%2C',',')
+
+url_final = url_fixo +''+ url_convert+''+url_entidade
+#print(url_final)
+requisicao = requests.get(url_final, verify=False, timeout=2000)
+requisicao_json = requisicao.json()
+print(len(requisicao_json['features']))
+#pprint.pprint(requisicao_json['features'][0]['geometry'], indent=4)
+
+tamanho_array = len(requisicao_json['features'])
+
+for i in range(tamanho_array):
+    #pprint.pprint(requisicao_json['features'][i]['geometry'], indent=4)
+    print('Evento',i,requisicao_json['features'][i]['geometry'])
