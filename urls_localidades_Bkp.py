f89ddf869c0856caaa7b295d1f7da582e0d011d5
10500007 <felipe_augusto.bn@hotmail.com>
2022-12-16 15:34:07 -0300

    atualização para web_v5

---
+++ urls_localidades_Bkp.py
@@ -0,0 +1,10 @@
+url_jari = '"modo":4,"datainicio":"2022-12-08 13:53:49","datatermino":"2022-12-12 14:53:49","valorpomenor":"0","valorpomaior":"0","valornemenor":"0","valornemaior":"0","filtrodans":true,"filtrodain":true,"filtrocopo":true,"filtrocone":true,"concessao":true,"dado":true,"exporta":0,"selecionado":{"elemelemento":"SE UHE Jari","elemcamadagis":"subestacaotemp","elemcampo":"nome","elembuffer":"5000","elemtipo":"1","elemns":"55","elemin":"66","elemto":"121","elemtipocampo":"0","elemdataanalise":"2020-01-01 00:00:00","elemtipoanalise":"0"},"GeoJSON":true,"GeoJSONBuffer":false'
+
+url_tocantins_2 = '"modo":4,"datainicio":"2022-12-16 01:28:03","datatermino":"2022-12-16 10:34:55","valorpomenor":"0","valorpomaior":"0","valornemenor":"0","valornemaior":"0","filtrodans":true,"filtrodain":true,"filtrocopo":true,"filtrocone":true,"concessao":true,"dado":false,"exporta":0,"selecionado":"{"elemelemento":"Tocantins","elemcamadagis":"estados","elemcampo":"nome","elembuffer":"0.00001","elemtipo":"0","elemns":"444","elemin":"858","elemto":"1.302","elemtipocampo":"0","elemdataanalise":"2020-01-01 00:00:00","elemtipoanalise":"0"},"GeoJSON":true,"GeoJSONBuffer":false'
+
+coord_peixe_angical = (-12.2311, -48.3891)
+coord_lajeado = (-9.7549, -48.3735)
+
+#print(url_tocantins)
+
+#url_tocantins = f'"modo":4,"datainicio":"'f'{data_ini}''","datatermino":"'f'{data_fim}''","valorpomenor":"0","valorpomaior":"0","valornemenor":"0","valornemaior":"0","filtrodans":true,"filtrodain":true,"filtrocopo":true,"filtrocone":true,"concessao":true,"dado":false,"exporta":0,"selecionado":"{"elemelemento":"Tocantins","elemcamadagis":"estados","elemcampo":"nome","elembuffer":"0.00001","elemtipo":"0","elemns":"444","elemin":"858","elemto":"1.302","elemtipocampo":"0","elemdataanalise":"2020-01-01 00:00:00","elemtipoanalise":"0"},"GeoJSON":true,"GeoJSONBuffer":false'
--- web_v5.py
+++ web_v5.py
@@ -1,25 +1,20 @@
 import requests
-import pprint
 import urllib.parse
-import urllib3.connection
 import urls_localidades
 from haversine import haversine
 
-
 url_fixo = 'https://www.netclima.com.br/php/hi/netclima_recuperahida.php?parametros={'
-url_parametros = urls_localidades.url_tocantins_2
+url_parametros = urls_localidades.url_tocantins
 url_entidade='}&entidade=EDP_T'
 
 encodedStr = url_parametros
 #conv = urllib.parse.unquote(encodedStr)
 #converter formato para link final da url
-url_convert = urllib.parse.quote(encodedStr).replace('%3A',':').replace('%2C',',')
-
-url_final = url_fixo +''+ url_convert+''+url_entidade
-#print(url_final)
+url_convert = url_convert = urllib.parse.quote(encodedStr).replace('%3A',':').replace('%2C',',').replace('%22%7B','{').replace('%7D','}')
+url_final = url_fixo + '' + url_convert + '' + url_entidade
 requisicao = requests.get(url_final, verify=False, timeout=2000)
 requisicao_json = requisicao.json()
-print(len(requisicao_json['features'])) #tamanho do array que contem eventos
+#print(len(requisicao_json['features'])) #tamanho do array que contem eventos
 #pprint.pprint(requisicao_json['features'][0]['geometry'], indent=4)
 
 tamanho_array = len(requisicao_json['features'])
@@ -33,11 +28,11 @@ for i in range(tamanho_array):
     coord_long = coord[2][0]   
     #print('Evento',i,requisicao_json['features'][i]['geometry']['coordinates'][0])
     #print('latitude',coord_lat,'longitude',coord_long)
-    peixe_angical = (-12.2311, -48.3891) # (lat, lon)
+    coord_usina = urls_localidades.coord_peixe_angical
     coord_do_event = (coord_lat, coord_long)
-    distancia_calculada = haversine(peixe_angical, coord_do_event)
+    distancia_calculada = haversine(coord_usina, coord_do_event)
     #print(distancia_calculada)
 
-    if distancia_calculada<750:
+    if distancia_calculada<300:
         print('Evento - ',contador,'-',distancia_calculada,'-',data_hora,'-',flag_nuvem)
         contador = contador + 1
